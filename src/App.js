41ad9fa95a9ccf5de771ce0899a204ce78c5f443
Brigham Andersen <brighamband@gmail.com>
2021-06-08 14:09:18 -0600

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppDiv = styled.div`
+  text-align: center;
+
+  .App-logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+`;
+
+const Card = styled.div`
+  background: lightgray;
+  width: 300px;
+  padding: 2rem;
+  margin: 1rem;
+`;
+
+const App = () => (
+  <AppDiv>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Card>
+        Testing
+      </Card>
+      <Card>
+        Testing
+      </Card>
+      <Card>
+        Testing
+      </Card>
+
+  </AppDiv>
+);
 
 export default App;
